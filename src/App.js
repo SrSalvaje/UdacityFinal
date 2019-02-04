9bfecda7a72813528d887f8aeef76ef35503731b
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-02-04 16:20:47 +0100

    chore: created Hello world, removed boilerplate, created basic compenent files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import {Route} from "react-router-dom"
-import Map from "./components/Map"
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-      <Map/>
+      <main>
+        <h1>Hello World</h1>
+      </main>
+      
 
     );
   }
