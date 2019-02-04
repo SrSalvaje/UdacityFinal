a2cbf57d5e9ed058b9909d714c185b1dc2afe1e1
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-02-04 22:23:43 +0100

    Give up, cant configure custom container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 //import {Route} from "react-router-dom";
-import Map from "./components/Map";
+import MapContainer from "./components/Map";
 import './App.css';
 
 
@@ -8,43 +8,14 @@ import './App.css';
 
 
 class App extends Component {
-  state = {
-    showingInfoWindow: false,  //Hides/shows  infoWindow
-    activeMarker: {},          //Shows active marker on click
-    selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
-  };
-
-  onMarkerClick = (props, marker, e) =>
-  this.setState({
-    selectedPlace: props,
-    activeMarker: marker,
-    showingInfoWindow: true
-  });
-
-  onClose = props => {
-    if (this.state.showingInfoWindow) {
-      this.setState({
-        showingInfoWindow: false,
-        activeMarker: null
-      });
-    }
-  };
+  
   
   render() {
+    
     return (
       <div className="App">
-        <h1 className="helloWorld">Hello Friends</h1>
-        
-          
-          <Map 
-          showingInfoWindow={this.state.showingInfoWindow}
-          activeMarker={this.state.activeMarker}
-          selectedPlace={this.state.selectedPlace}
-          onClose={this.onClose}
-          onMarkerClick={this.onMarkerClick}
-          />
-          
-        
+        <h1 className="helloWorld">My Malmö</h1>
+        <MapContainer/>
       </div>
       
 
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,42 +1,94 @@
 import React, { Component } from 'react';
-import styles from './Map.css';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
-import Markers from "./Marker.js"
+//import styles from './Map.css';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+import '../App.css';
+//import Markers from "./Marker.js"
 
 /* const mapStyles = {
     width: '100%',
-    height: '100%'
+    height: 'auto'
   }; */
-
+  
 export class MapContainer extends Component {
+
+    
+    state = {
+        showingInfoWindow: false,  //Hides or the shows the infoWindow
+        activeMarker: {},          //Shows the active marker upon click
+        selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
+      };
+
+      onMarkerClick = (props, marker, e) =>
+      this.setState({
+        selectedPlace: props,
+        activeMarker: marker,
+        showingInfoWindow: true
+      });
   
-  
-    render() {
-    return (
+        onClose = props => {
+        if (this.state.showingInfoWindow) {
+            this.setState({
+            showingInfoWindow: false,
+            activeMarker: null
+            });
+        }
+        };
+
    
-      <Map
-        google={this.props.google}
-        zoom={14}
-        style={styles.container}
-        initialCenter={{
-         lat: 55.609928,
-         lng: 12.999832
-        }}
-      >
-        <Markers
-        showingInfoWindow={this.props.showingInfoWindow}
-        activeMarker={this.props.activeMarker}
-        selectedPlace={this.props.selectedPlace}
-        onClose={this.props.onClose}
-        onMarkerClick={this.props.onMarkerClick}
-        />
-      </Map>
-    );
-  }
+
+    render(){
+    
+          
+        
+        return(
+            <Map 
+            google={this.props.google} 
+            zoom={14.5}
+            //style={mapStyles}
+            initialCenter={{
+                lat:55.610023, 
+                lng:13.000210
+            }}
+            >
+            {/* //add function to add markers here */}
+            <Marker
+                onClick={this.onMarkerClick}
+                name={'Malmö Central Station'}
+            />
+            <InfoWindow
+                marker={this.state.activeMarker}
+                visible={this.state.showingInfoWindow}
+                onClose={this.onClose}
+            >
+                <div>
+                    <h4>{this.state.selectedPlace.name}</h4>
+                </div>
+            </InfoWindow>
+                
+            {/* end marker adding code here */}
+            
+            </Map>
+
+        )
+    }
 }
 
+ const LContainer = (props) => (
+    <div  className="myMapCont" >Hello</div>
+  );
+
+/* export default GoogleApiWrapper({
+    apiKey: ("AIzaSyAn6EMjdX_667KspKuVZRvYDNEZHNQXKS4"),
+    LoadingContainer:LoadingContainer
+})( MapContainer);  */
+
+
+
+  
+
 export default GoogleApiWrapper(
- (props)=>({
-    apiKey: "AIzaSyAn6EMjdX_667KspKuVZRvYDNEZHNQXKS4"
- }
-))(MapContainer);
+    (LContainer) => ({
+      apiKey: ("AIzaSyAn6EMjdX_667KspKuVZRvYDNEZHNQXKS4"),
+      LoadingContainer:LContainer
+    }
+  ))(MapContainer)
