7e6ae6698c0802e35b0cc8ddb0e790056a3d203b
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-02-04 16:43:06 +0100

    feat: added styles and init function from Google map example

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,13 +3,32 @@ import '../App.css';
 
 
 class Map extends Component {
+    
+    //Google´s example
+    initMap=()=> {
+        // The location of Uluru
+        var uluru = {lat: -25.344, lng: 131.036};
+        // The map, centered at Uluru
+        var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: uluru});
+        // The marker, positioned at Uluru
+        var marker = new google.maps.Marker({position: uluru, map: map});
+      }
+
   render() {
     return (
-      <p>Map Goes here</p>
+      <div id="map"></div>
       
 
     );
   }
 }
-
+ initMap() {
+    // The location of Uluru
+    var uluru = {lat: -25.344, lng: 131.036};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: uluru});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
+  }
+  
 export default Map;
