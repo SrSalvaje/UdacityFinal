8633fb61803fc1fcfe1b7300319a5b52db4ed022
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-02-05 22:21:59 +0100

    forgot to commit on last

--- src/App.js
+++ src/App.js
@@ -48,20 +48,15 @@ class App extends Component {
     })
     .catch(error=>console.log(error))
   }
-
-  
   addMarkerToSate=(marker)=>{
     this.setState({markers:[...this.state.markers,marker]});
   }
   
   clickOnListItem=(vId)=>{
-    let infowindow = new window.google.maps.InfoWindow();
-
     const markerEquivalent=this.state.markers.filter(marker=>vId===marker.id)[0];
     //makes the marker bounce
     markerEquivalent.setAnimation(window.google.maps.Animation.BOUNCE);
     //for three seconds
-    
     window.setTimeout(function(){
       markerEquivalent.setAnimation(null);
     }, 3000);
