2001758fa0936716c090546f4ae85b508b272f55
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-02-05 02:55:31 +0100

    feat: working on fetch requests for foursquare

--- src/App.js
+++ src/App.js
@@ -1,14 +1,69 @@
 import React, { Component } from 'react';
 //import {Route} from "react-router-dom";
 import Map from "./components/Map";
-import Marker from "./components/Markers"
+//import Marker from "./components/Markers"
 import './App.css';
+import axios from 'axios'
 
 
 class App extends Component {
   state={
     markers:[]
   }
+
+  componentDidMount(){
+    //this.fetchVenues()
+    //this.axiosVenues()
+  }
+
+  //this funcytion uses the fetch api and throws a cors error
+   fetchVenues = ()=>{
+    const 
+      client_id="K4Z0NZV2MFC43ORGTT5ZLIDUQO3DVNUCLHIB33QS5W0KO54K",
+      client_secret="O0ZIQFPBW5GTWXJRUG2NFVHYR1HARB4RZCDEFRA0KXH55Y2V",
+      query="food",
+      near="Malmo,Sweden",
+      //limit="10",
+      v="20190204";
+     
+    const endPoint =`https://api.foursquare.com/v2/venues/explore?client_id=${client_id}&client_secret=${client_secret}&query=${query}&near=${near}&v=${v}`;
+    fetch(endPoint, {
+      headers:{
+        Authenthication:"user",
+       
+      
+      },
+      method:"GET"
+    }).then(response=>(console.log(response)))
+    .catch(error=>console.log(error))
+
+  } 
+  /* componentDidMount(){
+    this.getFoursquareVenues()
+  } */
+
+  //this function uses axios and works
+  getFoursquareVenues = () => {
+    const apiEndpoint = "https://api.foursquare.com/v2/venues/explore?"
+    const foursquareParameters = {
+        client_id: "K4Z0NZV2MFC43ORGTT5ZLIDUQO3DVNUCLHIB33QS5W0KO54K",
+        client_secret: "O0ZIQFPBW5GTWXJRUG2NFVHYR1HARB4RZCDEFRA0KXH55Y2V",
+        query: "food",
+        near: "Malmo, Sweden",
+        v: 20182507
+    }
+
+    axios.get(apiEndpoint + new URLSearchParams(foursquareParameters))
+        .then(response => {
+            
+            console.log(response)
+        })
+        .catch(error => {
+            alert("Error: Couldn't retrieve data from Foursquare.")
+            console.log(`An error occurred: ${error}`)
+        })
+}
+
   
   render() {
     return (
--- src/components/Markers.js
+++ src/components/Markers.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 //import {Route} from "react-router-dom";
 //import Map from "./components/Map";
 import '../App.css';
+import axios from "axios";
 
 
 class Marker extends Component {
@@ -26,4 +27,7 @@ export default Marker;
 /* Client ID
 K4Z0NZV2MFC43ORGTT5ZLIDUQO3DVNUCLHIB33QS5W0KO54K
 Client Secret
-O0ZIQFPBW5GTWXJRUG2NFVHYR1HARB4RZCDEFRA0KXH55Y2V */
+O0ZIQFPBW5GTWXJRUG2NFVHYR1HARB4RZCDEFRA0KXH55Y2V 
+GET https://api.foursquare.com/v2/venues/explore
+
+*/
