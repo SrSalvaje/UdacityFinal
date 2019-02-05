86f376a398bc642c9567ca2f4f6a7d59815ab1e0
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-02-05 11:55:27 +0100

    feat; added one marker

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+//import Marker from "./Markers"
 import '../App.css';
 
 
 class Map extends Component {
     
     componentDidMount(){
-        this.renderMap()
+        this.renderMap();
     }
 
+    
     renderMap =()=>{
         const APiKey="AIzaSyAn6EMjdX_667KspKuVZRvYDNEZHNQXKS4"
         loadMapScript(`https://maps.googleapis.com/maps/api/js?key=${APiKey}&callback=initMap`);
@@ -16,17 +18,33 @@ class Map extends Component {
     //Google´s example
     initMap=()=> {
        
-      /*   const map = */ new window.google.maps.Map(document.getElementById("map"), {
+        const map = new window.google.maps.Map(document.getElementById("map"), {
             center:{lat: 55.609126, lng: 13.000811},
             zoom:14
-        })
-         
+        });
+        //adapted from https://developers.google.com/maps/documentation/javascript/markers 
+        let marker = new window.google.maps.Marker({
+            position: {lat: 55.609126, lng: 13.000811},
+            map: map,
+            title: 'Central Station'
+          });
+         this.props.venues.map(venue=>{
+            let marker = new window.google.maps.Marker({
+                position: {lat: venue.venue.location.lat, lng: venue.venue.location.lng},
+                map: map
+                //title: 'Central Station'
+              });
+
+         })
       }
 
   render() {
     return (
     <div class="mapContainer">
-        <div id="map"></div>
+        <div id="map">
+        {/* here is where i should write my loops for rndering all markers */}
+        
+        </div>
     </div>
       
       
