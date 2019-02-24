47333c96fe18db73c308b0a942cad015d6ee6705
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-02-24 20:36:48 +0100

    feat: added skip link to venues

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -31,7 +31,7 @@ class SearchBar extends Component {
           
     return (
     <div className="side-bar">
-        <select className="searchCont"  /* value={query} */  onChange={(e)=>this.changeCat(e.target)} >
+        <select className="searchCont" id="searchBar"  /* value={query} */  onChange={(e)=>this.changeCat(e.target)} >
             <option value={query} >{query ? query : "Top Picks"}</option>
             
              {this.props.categories.map(cat=>{
@@ -50,6 +50,7 @@ class SearchBar extends Component {
                         <p className="lName"  aria-label="Name" tabIndex="0">{venue.venue.name}</p>
                         <p className="lCat"  aria-label="Type" tabIndex="0">{venue.venue.categories[0].name ? venue.venue.categories[0].name : "No Category Available"}</p> 
                         <p className="lAdd"  aria-label="Address" tabIndex="0" >{`Address: ${venue.venue.location.address}, ${venue.venue.location.city}`}</p>
+                        <a tabindex="0" href="#searchBar" class="skip-link">Go back to search bar</a>
                     </div>      
                 </li>
                 ))
