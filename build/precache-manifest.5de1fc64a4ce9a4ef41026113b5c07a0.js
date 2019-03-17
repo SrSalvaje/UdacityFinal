c3b2687e6ad0e207be9c6fe5683fb8413bf76823
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-03-17 19:31:57 +0100

    production build

---
+++ build/precache-manifest.5de1fc64a4ce9a4ef41026113b5c07a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f45b0a12aaeb923b30eb098d8377ac11",
+    "url": "/static/media/pbf.f45b0a12.png"
+  },
+  {
+    "revision": "d2bd1f98b6a7d95d7ce117a588584e8c",
+    "url": "/static/media/google.d2bd1f98.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0750b40407abfd6407d0",
+    "url": "/static/js/main.0750b404.chunk.js"
+  },
+  {
+    "revision": "5bd71c295407fbf302cc",
+    "url": "/static/js/1.5bd71c29.chunk.js"
+  },
+  {
+    "revision": "0750b40407abfd6407d0",
+    "url": "/static/css/main.8f405a88.chunk.css"
+  },
+  {
+    "revision": "a926edaca3f864cde291685085a6bd42",
+    "url": "/index.html"
+  }
+];
